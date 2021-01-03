7ca2ac14bee7b9fc5caac4e8e1ea19cd00a4b280
Marta - nowe <martaowczarczuk@gmail.com>
2021-01-03 12:40:55 +0100

    Exoplayer error listener added

--- app/src/main/java/com/example/videodemo/view/ui/VideoPlayerActivity.java
+++ app/src/main/java/com/example/videodemo/view/ui/VideoPlayerActivity.java
@@ -4,17 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.videodemo.R;
+import com.google.android.exoplayer2.ExoPlaybackException;
 import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.ui.StyledPlayerView;
 
 public class VideoPlayerActivity extends AppCompatActivity {
 
-    String url;
-    SimpleExoPlayer player;
-    StyledPlayerView playerView;
+    private String TAG = "ExoPlayer";
+    private String url;
+    private SimpleExoPlayer player;
+    private StyledPlayerView playerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +26,8 @@ public class VideoPlayerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_video_player);
         getValueFromIntent();
         attributeWidgets();
-
-        player = new SimpleExoPlayer.Builder(this).build();
-        playerView.setPlayer(player);
-        // Build the media item.
-        MediaItem mediaItem = MediaItem.fromUri(url);
-// Set the media item to be played.
-        player.setMediaItem(mediaItem);
-// Prepare the player.
-        player.prepare();
-// Start the playback.
-        player.play();
+        setExoPlayer();
+        setExoPlayersListeners();
     }
 
     private void getValueFromIntent() {
@@ -42,5 +37,34 @@ public class VideoPlayerActivity extends AppCompatActivity {
 
     private void attributeWidgets() {
         playerView = findViewById(R.id.player_view);
+    }
+
+    private void setExoPlayer() {
+        player = new SimpleExoPlayer.Builder(this).build();
+        playerView.setPlayer(player);
+        player.setMediaItem(MediaItem.fromUri(url));
+        player.prepare();
+        player.play();
+    }
+
+    private void setExoPlayersListeners() {
+        player.addListener(new Player.EventListener() {
+            @Override
+            public void onPlayerError(ExoPlaybackException error) {
+                switch (error.type) {
+                    case ExoPlaybackException.TYPE_SOURCE:
+                        Log.e(TAG, "TYPE_SOURCE: " + error.getSourceException().getMessage());
+                        break;
+
+                    case ExoPlaybackException.TYPE_RENDERER:
+                        Log.e(TAG, "TYPE_RENDERER: " + error.getRendererException().getMessage());
+                        break;
+
+                    case ExoPlaybackException.TYPE_UNEXPECTED:
+                        Log.e(TAG, "TYPE_UNEXPECTED: " + error.getUnexpectedException().getMessage());
+                        break;
+                }
+            }
+        });
     }
 }
